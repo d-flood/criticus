8c82ba4a21edd2b882465f07372f4953b2f07ba2
David Flood <davidfloodii@gmail.com>
2022-02-25 07:26:13 -0600

    differentiated rdgs enumerated per parent

--- criticus/py/reformat_collation/differentiate_subreading_ids.py
+++ criticus/py/reformat_collation/differentiate_subreading_ids.py
@@ -1,4 +1,14 @@
+from enum import unique
 from lxml import etree as et
+
+def version_rdgs(elem: et._Element, regularized: dict):
+    if elem.get('n') in regularized:
+        regularized[elem.get('n')] += 1
+    else:
+        regularized[elem.get('n')] = 1
+    unique_name = f'{elem.get("n")}{regularized[elem.get("n")]}'
+    elem.attrib['n'] = unique_name
+    return regularized, unique_name
 
 def differentiate_subreading_ids(xml_filename) -> et._ElementTree:
     ns = '{http://www.tei-c.org/ns/1.0}'
@@ -7,24 +17,24 @@ def differentiate_subreading_ids(xml_filename) -> et._ElementTree:
     root = xml.getroot()
     for ab in root:
         for app in ab:
-            n_num = 1
+            regularized = {}
             for elem in app:
                 if elem.tag == f'{ns}rdg' and 'r' in elem.get('n'):
-                    elem.attrib['n'] = f'{elem.get("n")}{n_num}'
-                    n_num += 1
+                    regularized, _ = version_rdgs(elem, regularized)
+
                 elif elem.tag == f'{ns}note': # this is a <note> element
                     for child in elem: # this is a <note> child
                         if child.tag == f'{ns}graph':
-                            node_num = 1
+                            regularized = {}
                             for node in child:
                                 if node.tag == f'{ns}node':
                                     if 'r' in node.get('n'):
-                                        distinct_attrib = f'{node.get("n")}{node_num}'
-                                        node.attrib['n'] = distinct_attrib
+                                        regularized, distinct_attrib = version_rdgs(node, regularized)
+                                        # distinct_attrib = f'{node.get("n")}{node_num}'
+                                        # node.attrib['n'] = distinct_attrib
                                         arc = et.Element('arc')
                                         arc.attrib['from'] = distinct_attrib[0]
                                         arc.attrib['to'] = distinct_attrib
                                         node.getparent().append(arc)
-                                        node_num += 1
     # xml.write(xml_filename, encoding='utf8')
     return xml
